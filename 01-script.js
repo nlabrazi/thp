30c0dbe1ceaa8a55ff978b8675a51a95609be76b
Nabil Labrazi <na.labrazi@gmail.com>
2021-01-14 02:06:14 +0100

    almost done

--- 01-script.js
+++ 01-script.js
@@ -1,117 +1,155 @@
-console.log("Require file");
 console.log("");
+const fs = require('fs');
+const checkSpecialCharacters = /[^0-9-\+\s]+/g;
+const fileName = process.argv[2];
 
 
-// const fs = require('fs');
-// const fileName = process.argv[2];
-
-// // Méthode asynchrone
-// fs.readFile(fileName, 'utf8', (error, data) => {
-//     if (error) {
-//         console.error(error.message);
-//         return ;
-//     }
-//     console.log(data);
-// });
-
-// // Méthode synchrone
-// try {
-//     const data = fs.readFileSync(fileName, 'utf8');
-//     console.log(data);
-// } catch (error) {
-//     console.error(error.message);
-// }
+let bubbleCount = 0;
+let insertCount = 0;
+let selectCount = 0;
+let rapidCount = 0;
 
 
 // Tri à bulle
-var arrayData = [48, -2, 6, 12, 0, -4];
-
-const sortBuble = (arrayData) => {
-  let isSorted = false;
-  while (!isSorted) {
-    isSorted = true;
-    for (let i = 0; i < arrayData.length -1; i++) {
-     if (arrayData[i+1] < arrayData[i]) {
-      [arrayData[i], arrayData[i + 1]] = [arrayData[i + 1], arrayData[i]];
-      isSorted = false;
+const sortBuble = (array) => {
+  let copyArray = [...array];
+  let n = copyArray.length - 1;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      bubbleCount += 1;
+      if (copyArray[j] < copyArray[j+1]) {
+        [copyArray[j], copyArray[j + 1]] = [copyArray[j + 1], copyArray[j]];
+      }
     }
+    return copyArray;
   }
-}
-return arrayData;
-}
-console.log("Tri à bulle: " + sortBuble(arrayData));
-
+};
 
 
 // Tri par insertion
-var arrayData2 = [48, -2, 6, 12, 0, -4];
-
-const sortInsertion = (arrayData2) => {
-  n = arrayData2.length;
+const sortInsertion = (array) => {
+  let n = array.length;
   for (let i = 1; i <= n-1; i++) {
-    x = arrayData2[i];
-    j = i;
-    while (j > 0 && arrayData2[j - 1] > x) {
-      arrayData2[j] = arrayData2[j - 1];
+    let x = array[i];
+    let j = i;
+    while (j > 0 && array[j - 1] > x) {
+      insertCount += 1;
+      array[j] = array[j - 1];
       j-=1;
     }
-    arrayData2[j] = x;
+    array[j] = x;
   }
-  return arrayData2;
-}
-console.log("Tri par insertion : " + sortInsertion(arrayData2));
-
+  return array;
+};
 
 
 // Tri par sélection
-var arrayData3 = [48, -2, 6, 12, 0, -4];
-
-const sortSelection = (arrayData3) => {
-  n = arrayData3.length;
+const sortSelection = (array) => {
+  let copyArray = [...array];
+  let n = array.length;
   for (let i = 0; i < n; i++) {
     let mini = i;
     for (let j = i+1; j < n; j++) {
-      if (arrayData3[j] < arrayData3[mini]) {
+      selectCount += 1;
+      if (copyArray[j] < copyArray[mini]) {
         mini = j;
       }
     }
     if (mini !== i) {
-      [arrayData3[i], arrayData3[mini]] = [arrayData3[mini], arrayData3[i]];
+      [copyArray[i], copyArray[mini]] = [copyArray[mini], copyArray[i]];
     }
   }
-  return arrayData3;
-}
-console.log("Tri par selection : " + sortSelection(arrayData3));
+  return copyArray;
+};
 
 
-
-// Tri rapide
-var arr = [48, -2, 6, 12, 0, -4];
-
-  let swap = (arr, i, j) => {
-    let tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-  };
-  let partition = (arr, low, high) => {
-    let q = low, i;
-    for (i = low; i < high; i++) {
-      if (arr[i] < arr[high]) {
-        swap(arr, i, q);
-        q++;
+// Tri rapid
+const sortRapid = (array, low = 0, high = array.length -1) => {
+  let partition = (array, low, high) => {
+    let swap = array[Math.floor((high + low) / 2)];
+    while (low <= high) {
+      while (array[low] > swap) {
+        rapidCount += 1;
+        low++;
+      }
+      while (array[high] < swap) {
+        rapidCount += 1;
+        high--;
+      }
+      if (low <= high) {
+        rapidCount += 1;
+        [array[low], array[high]] = [array[high], array[low]];
+        low++;
+        high--;
       }
     }
-    swap(arr, i, q);
-    return q;
+    return low;
   };
-  let sortRapide = (arr, low, high) => {
-    if (low < high) {
-      let pivot = partition(arr, low, high);
-      sortRapide(arr, low, pivot - 1);
-      sortRapide(arr, pivot + 1, high);
-      return arr;
+  let n = array.length;
+  if (n > 1 ) {
+    let index = partition(array, low, high);
+    if (low < index -1) {
+      sortRapid(array, low, index - 1);
     }
-  };
-  return arr;
+    if (index < high) {
+      sortRapid(array, index, high);
+    }
+  }
+  return array;
+};
 
-console.log("Tri rapide : " + sortRapide(arr));
+
+// general
+const openFileWithCatch = (error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(`${fileName} ouvert...`);
+};
+
+const parseArrayCatch = (data) => {
+  if (checkSpecialCharacters.test(data)) {
+    console.log("Characteres spéciaux trouvé. Suppression...");
+  }
+  let refArray = Array.from(data.replace(checkSpecialCharacters, "").split(" "));
+  return refArray.map((x) => +x);
+};
+const printFinalArray = (array) => {
+  console.log("Tableau original ci-dessous :");
+  console.log(array);
+  console.log("");
+};
+
+const displayAllResults = (array) => {
+  const bubleSort = sortBuble([...array]);
+  const insertSort = sortInsertion([...array]);
+  const selectionSort = sortSelection([...array]);
+  const rapidSort = sortRapid([...array]);
+
+  const resultString = (strategy, result, count) => {
+    return `Tri par ${strategy} => [${result}] avec ${count} comparaisons`;
+  };
+
+
+// Output
+console.log("******************************");
+console.log("**                          **");
+console.log("**     Résultats finaux     **");
+console.log("**                          **");
+console.log("******************************");
+console.log("");
+console.log(resultString("Bulle", bubleSort, bubbleCount));
+console.log(resultString("Insertion", insertSort, insertCount));
+console.log(resultString("Selection", selectionSort, selectCount));
+console.log(resultString("Rapide", rapidSort, rapidCount));
+};
+
+
+// Méthode asynchrone
+fs.readFile(fileName, 'utf8', (error, data) => {
+  openFileWithCatch(error);
+  array = parseArrayCatch(data);
+  printFinalArray(array);
+  displayAllResults(array);
+});
