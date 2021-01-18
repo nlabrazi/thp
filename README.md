59a3a837b94ef19629245e1996293dd7ad8343da
Nabil Labrazi <na.labrazi@gmail.com>
2021-01-18 11:56:01 +0100

    week3-day1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Projet THP-NEXT : WEEK 2 --- DAY 4-5
-# LES ZARBRES !
+# Projet THP-NEXT : WEEK 3 --- DAY 1
+# Manipulations DOM !
 
 By Nabil Simon
 
--- index.js
+++ index.js
@@ -1,219 +1,36 @@
-class Node {
-  constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
-  }
+let tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
+let tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+function showPanel(panelIndex, colorCode) {
+  tabButtons.forEach(function(node) {
+    node.style.backgroundColor="";
+    node.style.color="";
+  });
+  tabButtons[panelIndex].style.backgroundColor=colorCode;
+  tabButtons[panelIndex].style.color="white";
+
+  tabPanels.forEach(function(node) {
+    node.style.display="none";
+  });
+  tabPanels[panelIndex].style.display="block";
+//  tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
 
-class BST {
-  constructor(value) {
-    this.root = new Node(value)
-    this.count = 1
-  }
-
-  size() {
-    return this.count
-  }
-
-  insert(value) {
-    this.count++
-
-    let newNode = new Node(value)
-    //recursive search function
-    const searchTree = node => {
-      // if value < node.value, go left
-      if (value < node.value) {
-        //if no left child, append new node
-        if (!node.left) {
-          node.left = newNode
-        }
-        // if left child, look left again (recursive case)
-        else {
-          searchTree(node.left)
-        }
-      }
-      // if value < node.value, go left
-      else if (value > node.value) {
-        if (!node.right) {
-          node.right = newNode
-        }
-        // if right child, look right again
-        else {
-          searchTree(node.right)
-        }
-      }
-    }
-
-
-    searchTree(this.root)
-  }
-
-  min() {
-    let currentNode = this.root
-
-    // continue traversing left until no more children
-    while (currentNode.left) {
-      currentNode = currentNode.left
-    }
-
-    return currentNode.value
-  }
-
-  max() {
-    let currentNode = this.root
-
-    // continue traversing right until no more children
-    while (currentNode.right) {
-      currentNode = currentNode.right
-    }
-
-    return currentNode.value
-  }
-
-  contains(value) {
-    let currentNode = this.root
-
-    while (currentNode) {
-      if (value === currentNode.value) {
-        return true
-      }
-      if (value < currentNode.value) {
-        currentNode = currentNode.left
-      } else {
-        currentNode = currentNode.right
-      }
-    }
-
-    return false;
-  }
-
-  // depth first search - looking branch by branch
-
-  // in-order
-  // left, root, right
-  // 2, 3, 12, 15, 28, 36, 39
-  dfsInOrder() {
-    let result = []
-    // nasted recursive function
-    const traverse = node => {
-    // if left child exists, go left again
-    if (node.left) traverse(node.left)
-    // capture root node value
-    result.push(node.value)
-    // if left child exists, go right again
-    if (node.right) traverse(node.right)
-    }
-
-  traverse(this.root)
-
-  return result
-}
-
-  // pre-order
-  // root, left, right
-  // 15, 3, 2, 12, 36, 28, 39
-  dfsPreOrder() {
-    let result = []
-    // recursive traverse function
-    const traverse = node => {
-    // capture root node value
-    result.push(node.value)
-    // if left child exists, go left again
-    if (node.left) traverse(node.left)
-    // if left child exists, go right again
-    if (node.right) traverse(node.right)
-    }
-
-  traverse(this.root)
-
-  return result
-}
-
-  // post-order
-  // left, right, root
-  // 2, 12, 3 , 28, 39, 36, 15
-  dfsPostOrder() {
-    let result = []
-    // recursive traverse function
-    const traverse = node => {
-    // if left child exists, go left again
-    if (node.left) traverse(node.left)
-    // if left child exists, go right again
-    if (node.right) traverse(node.right)
-    // capture root node value
-    result.push(node.value)
-    }
-
-  traverse(this.root)
-
-  return result
-}
-
-  // breadth first search - looking level by level
-
-  // use a queue !
-  // 15, 3, 36, 2, 12, 28, 39
-  bfs() {
-    let result = []
-    let queue = []
-
-    queue.push(this.root)
-    // take first item up to the queue
-    while(queue.length) {
-      let currentNode = queue.shift()
-      // take item of the queue & put in to the result
-      result.push(currentNode.value)
-
-      if (currentNode.left) {
-        queue.push(currentNode.left)
-      }
-      if (currentNode.right) {
-        queue.push(currentNode.right)
-      }
-    }
-
-    return result
-  }
-}
-
-const bst = new BST(15)
-
-bst.insert(3)
-bst.insert(36)
-bst.insert(2)
-bst.insert(12)
-bst.insert(28)
-bst.insert(39)
-
-
-console.log("**********************");
-console.log("**                  **");
-console.log("**       OUTPUT     **");
-console.log("**                  **");
-console.log("**********************");
-console.log("");
-
-console.log("Size tree: " + bst.size());
-console.log("Tree min value: " + bst.min());
-console.log("Tree max value: " + bst.max());
-
-console.log("Does tree contain 2?: " + bst.contains(2));
-console.log("Does tree contain 9?: " + bst.contains(9));
-
-console.log("Tree sort by in-order (left, root, right) : " + bst.dfsInOrder());
-console.log("Tree sort by pre-order (root, left, right) : " + bst.dfsPreOrder());
-console.log("Tree sort by post-order (left, right, root) : " + bst.dfsPostOrder());
-
-console.log("Level by Level search : " + "["+bst.bfs()+"]"); //feelsbadtricking!
+showPanel(0, '#f44336');
 
 
 
-//      BFS EXAMPLE
-//          15
-//         /  \
-//        03   36
-//       / \   / \
-//      2  12 28  39
-//     / \    /
-//    xx  x  xx
+var main_courses = ["Filet de turbot de la mer Noire", "Tablier de sapeur", "Gigot d'agneau", "Faisan de forêt", "Trio de quinoa, chou kale et pousses d'épinard"]
+var techniques = ["à la cocotte", "minute", "avec sa sauce hollandaise", "façon sud-ouest", "comme chez ma grand-mère", "déglacé au saké", "maturé en fût de chêne"]
+var sides = ["une purée de topinambour", "ses frites truffées", "des châtaignes croustillantes", "une brunoise carotte-cèleri", "un oeuf parfait", "sa crème veloutée de fromages affinés"]
+var seasonings = ["au yuzu rouge", "au poivre vert de Sichuan", "et une pointe de safran", "à l'ail noir", "et un peu de sucre en poudre"]
+
+var random_main_course = main_courses[Math.floor(Math.random()*main_courses.length)]
+var random_technique = techniques[Math.floor(Math.random()*techniques.length)]
+var random_side = sides[Math.floor(Math.random()*sides.length)]
+var random_seasoning = seasonings[Math.floor(Math.random()*seasonings.length)]
+
+var menu = `${random_main_course} ${random_technique}, avec ${random_side} ${random_seasoning}`
+
+let tab2 = document.getElementById("tab2");
+tab2.addEventListener("onclick", ())
